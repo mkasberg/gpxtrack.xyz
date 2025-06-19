039f53382dbcd4311b4873321e62197dab087ad0
Mike Kasberg <kasberg.mike@gmail.com>
2025-06-19 12:05:45 -0600

    Fix typeof Manifold TypeScript error

--- src/manifold-instance.ts
+++ src/manifold-instance.ts
@@ -2,9 +2,9 @@ import Module from 'manifold-3d';
 import type { Manifold, CrossSection } from 'manifold-3d';
 
 // Single promise that will be reused for all initialization requests
-let initPromise: Promise<{ Manifold: typeof Manifold; CrossSection: typeof CrossSection }> | null = null;
+let initPromise: Promise<{ Manifold: any; CrossSection: any }> | null = null;
 
-export async function getManifoldInstance() {
+export async function getManifoldInstance(): Promise<{ Manifold: any; CrossSection: any }> {
   if (!initPromise) {
     // Create the promise only once
     initPromise = (async () => {
