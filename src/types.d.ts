0798aeaa122eaabf5ff5f74d448f934ddd9e3923
Mike Kasberg <kasberg.mike@gmail.com>
2025-06-19 12:14:21 -0600

    Fix font module import error with TypeScript declarations

---
+++ src/types.d.ts
@@ -0,0 +1,69 @@
+// TypeScript declarations for asset imports
+
+// Font files
+declare module '*.woff' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.woff2' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.ttf' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.otf' {
+  const src: string;
+  export default src;
+}
+
+// Image files
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
+
+// Other common asset types
+declare module '*.css' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.scss' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.json' {
+  const content: any;
+  export default content;
+}
